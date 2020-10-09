aab690171d6c958553c8611c690c169e50b72839
Onibenjo <onibenjo@gmail.com>
2020-10-09 16:49:33 +0100

    added twilio video

--- src/components/TwilioVideoDisplay.js
+++ src/components/TwilioVideoDisplay.js
@@ -6,6 +6,8 @@ import {
   TouchableOpacity,
   PermissionsAndroid,
   TouchableHighlight,
+  TextInput,
+  Button,
 } from 'react-native';
 import {
   TwilioVideoLocalView,
@@ -47,7 +49,7 @@ const TwilioVideoDisplay = () => {
     token: '',
   });
 
-  const twilioVideoRef = useRef(null);
+  const twilioVideoRef = useRef();
 
   useEffect(() => {
     getAllPermissions();
@@ -218,7 +220,150 @@ const TwilioVideoDisplay = () => {
   );
 };
 
-export default TwilioVideoDisplay;
+export {TwilioVideoDisplay};
+
+export default class Example extends React.Component {
+  state = {
+    isAudioEnabled: true,
+    isVideoEnabled: true,
+    status: 'disconnected',
+    participants: new Map(),
+    videoTracks: new Map(),
+    token: '',
+  };
+
+  _onConnectButtonPress = () => {
+    this.refs.twilioVideo.connect({accessToken: this.state.token});
+    this.setState({status: 'connecting'});
+  };
+
+  _onEndButtonPress = () => {
+    this.refs.twilioVideo.disconnect();
+  };
+
+  _onMuteButtonPress = () => {
+    this.refs.twilioVideo
+      .setLocalAudioEnabled(!this.state.isAudioEnabled)
+      .then((isEnabled) => this.setState({isAudioEnabled: isEnabled}));
+  };
+
+  _onFlipButtonPress = () => {
+    this.refs.twilioVideo.flipCamera();
+  };
+
+  _onRoomDidConnect = ({roomName, error}) => {
+    console.log('onRoomDidConnect: ', roomName);
+
+    this.setState({status: 'connected'});
+  };
+
+  _onRoomDidDisconnect = ({roomName, error}) => {
+    console.log('ERROR: ', error);
+
+    this.setState({status: 'disconnected'});
+  };
+
+  _onRoomDidFailToConnect = (error) => {
+    console.log('ERROR: ', error);
+
+    this.setState({status: 'disconnected'});
+  };
+
+  _onParticipantAddedVideoTrack = ({participant, track}) => {
+    console.log('onParticipantAddedVideoTrack: ', participant, track);
+
+    this.setState({
+      videoTracks: new Map([
+        ...this.state.videoTracks,
+        [
+          track.trackSid,
+          {participantSid: participant.sid, videoTrackSid: track.trackSid},
+        ],
+      ]),
+    });
+  };
+
+  _onParticipantRemovedVideoTrack = ({participant, track}) => {
+    console.log('onParticipantRemovedVideoTrack: ', participant, track);
+
+    const videoTracks = new Map(this.state.videoTracks);
+    videoTracks.delete(track.trackSid);
+
+    this.setState({videoTracks});
+  };
+
+  render() {
+    return (
+      <View style={styles.container}>
+        {this.state.status === 'disconnected' && (
+          <View>
+            <Text style={styles.welcome}>React Native Twilio Video</Text>
+            <TextInput
+              style={styles.input}
+              autoCapitalize="none"
+              value={this.state.token}
+              onChangeText={(text) => this.setState({token: text})}></TextInput>
+            <Button
+              title="Connect"
+              style={styles.button}
+              onPress={this._onConnectButtonPress}></Button>
+          </View>
+        )}
+
+        {(this.state.status === 'connected' ||
+          this.state.status === 'connecting') && (
+          <View style={styles.callContainer}>
+            {this.state.status === 'connected' && (
+              <View style={styles.remoteGrid}>
+                {Array.from(
+                  this.state.videoTracks,
+                  ([trackSid, trackIdentifier]) => {
+                    return (
+                      <TwilioVideoParticipantView
+                        style={styles.remoteVideo}
+                        key={trackSid}
+                        trackIdentifier={trackIdentifier}
+                      />
+                    );
+                  },
+                )}
+              </View>
+            )}
+            <View style={styles.optionsContainer}>
+              <TouchableOpacity
+                style={styles.optionButton}
+                onPress={this._onEndButtonPress}>
+                <Text style={{fontSize: 12}}>End</Text>
+              </TouchableOpacity>
+              <TouchableOpacity
+                style={styles.optionButton}
+                onPress={this._onMuteButtonPress}>
+                <Text style={{fontSize: 12}}>
+                  {this.state.isAudioEnabled ? 'Mute' : 'Unmute'}
+                </Text>
+              </TouchableOpacity>
+              <TouchableOpacity
+                style={styles.optionButton}
+                onPress={this._onFlipButtonPress}>
+                <Text style={{fontSize: 12}}>Flip</Text>
+              </TouchableOpacity>
+              <TwilioVideoLocalView enabled={true} style={styles.localVideo} />
+            </View>
+          </View>
+        )}
+
+        <TwilioVideo
+          ref="twilioVideo"
+          onRoomDidConnect={this._onRoomDidConnect}
+          onRoomDidDisconnect={this._onRoomDidDisconnect}
+          onRoomDidFailToConnect={this._onRoomDidFailToConnect}
+          onParticipantAddedVideoTrack={this._onParticipantAddedVideoTrack}
+          onParticipantRemovedVideoTrack={this._onParticipantRemovedVideoTrack}
+        />
+      </View>
+    );
+  }
+}
 
 const styles = StyleSheet.create({
   container: {
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,11 +1,14 @@
 import React from 'react';
-import {StyleSheet, Image} from 'react-native';
+import {StyleSheet, Image, View} from 'react-native';
 import FormButton from '../components/Button/FormButton';
 import screens from '../helpers/screens';
+import tailwind from 'tailwind-rn';
+import {useAuth} from '../context/auth';
 
 const HomeScreen = ({navigation}) => {
+  const {logout} = useAuth();
   return (
-    <>
+    <View style={tailwind('px-5')}>
       <Image
         source={require('../assets/text_re.png')}
         resizeMode="contain"
@@ -15,7 +18,8 @@ const HomeScreen = ({navigation}) => {
         title="Video Chat"
         onPress={() => navigation.navigate(screens.videoChat)}
       />
-    </>
+      <FormButton title="Log Out" onPress={logout} />
+    </View>
   );
 };
 
